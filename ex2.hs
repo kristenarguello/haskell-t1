81e8bffb2a06b24c3dda359c7b1cd9e2d2b27b73
Kristen <kristenarguello20@gmail.com>
2023-09-16 08:24:16 -0300

    Exercicio 1 e 2

--- ex2.hs
+++ ex2.hs
@@ -2,3 +2,9 @@
 -- desejado e retorna o valor equivalente em binário (interpretado como número inteiro sem sinal) com o
 -- número de bits informado. Por exemplo, 𝑑𝑒𝑐2𝑏𝑖𝑛 2 8 deve retornar [0,0,0,0,0,0,1,0]. 𝑑𝑒𝑐2𝑏𝑖𝑛 ∷ 𝐼𝑛𝑡 →
 -- 𝐼𝑛𝑡 → [𝐼𝑛𝑡]
+
+dec2bin :: Int -> Int -> [Int]
+dec2bin 0 0 = []
+dec2bin v t = if v - (2 ^ (t-1)) < 0
+                then 0 : dec2bin v (t-1)
+                else 1 : dec2bin (v-(2 ^ (t-1))) (t-1)
